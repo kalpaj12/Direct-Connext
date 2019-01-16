98041d1c9777b7af6dd25d8fcf97f61eed56b2d2
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2019-01-17 00:41:00 +0530

    PHP layer for webpage display of available files in the server

---
+++ ServerFiles/PHP_Layer_Refresh.php
@@ -0,0 +1,42 @@
+<?php
+ $url1=$_SERVER['REQUEST_URI'];
+ header("Refresh: 1; URL=$url1");
+$host    = "localhost";
+$user    = "root";
+$pass    = "kalpaj@123";
+$db_name = "dc";
+
+//create connection
+$connection = mysqli_connect($host, $user, $pass, $db_name);
+
+//test if connection failed
+if(mysqli_connect_errno()){
+    die("connection failed: "
+        . mysqli_connect_error()
+        . " (" . mysqli_connect_errno()
+        . ")");
+}
+
+//get results from database
+$result = mysqli_query($connection,"SELECT * FROM filenames");
+$all_property = array();  //declare an array for saving property
+
+//showing property
+echo '<table class="data-table">
+        <tr class="data-heading">';  //initialize table tag
+while ($property = mysqli_fetch_field($result)) {
+    echo '<td>' . $property->name . '</td>';  //get field name for header
+    array_push($all_property, $property->name);  //save those to array
+}
+echo '</tr>'; //end tr tag
+
+//showing all data
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    foreach ($all_property as $item) {
+        echo '<td>' . $row[$item] . '</td>'; //get items using property value
+    }
+    echo '</tr>';
+}
+echo "</table>";
+?>
